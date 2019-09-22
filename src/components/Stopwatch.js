6aa6bb236d707f9b27f7c3be8fb5a464a060f0ac
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-22 19:29:03 -0400

    adds h1 StopWatch to stopwatch component

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Stopwatch(props) {
     return(
         <div>
+            <h1>StopWatch</h1>
             <p>{ props.hours + " hours"}</p>
             <p>{ props.minutes + " minutes"}</p>
             <p>{ props.seconds + " seconds"}</p>
