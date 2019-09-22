5d4b3ec605f72c1c245d70d65062e3ea95f3a603
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-22 19:24:26 -0400

    rearranges hours minutes and seconds in that order

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Stopwatch(props) {
     return(
         <div>
-            <p>{ props.seconds }</p>
-            <p>{ props.minutes }</p>
             <p>{ props.hours }</p>
+            <p>{ props.minutes }</p>
+            <p>{ props.seconds }</p>
         </div>
     )
 } 
