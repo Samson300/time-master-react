08bbb4a7baf39db7ca1c0116bf603c7575080fe9
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-23 17:34:16 -0400

    adds h1 to Countdown component

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -3,6 +3,7 @@ import React from "react"
 export default function CountDown(props) {
     return (
         <div>
+            <h1>CountDown</h1>
             <p>{props.countdown}</p>
         </div>
     )
